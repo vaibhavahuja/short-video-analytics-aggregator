bccae95d391330c10b53c23ef8ea66aa0572ac97
Vaibhav Ahuja <vaibhavahuja@outlook.com>
2024-09-11 22:56:58 +0530

    minor refactoring in unit test

--- internal/app/handlers/eventHandler_test.go
+++ internal/app/handlers/eventHandler_test.go
@@ -13,9 +13,9 @@ import (
 
 type TestCase struct {
 	Name   string
-	input  any
-	output any
-	err    error
+	Input  any
+	Output any
+	Err    error
 }
 
 func TestHandleMessage(t *testing.T) {
@@ -24,64 +24,64 @@ func TestHandleMessage(t *testing.T) {
 	testCases := []TestCase{
 		{
 			Name: "handle_event_success",
-			input: queue.Message{
+			Input: queue.Message{
 				Value: []byte(fmt.Sprintf("{\"video_id\":\"asdsa\",\"video_title\":\"Amazing Adventure\",\"genres\":[\"Action\",\"Adventure\"],\"user_id\":12345,\"platform\":\"YouTube\",\"duration\":3600,\"timestamp\":\"%d\",\"video_quality\":\"4K\"}", time.Now().Unix())),
 				Topic: "random",
 			},
-			err: nil,
+			Err: nil,
 		},
 		{
 			Name: "handle_event_invalid_event_1",
-			input: queue.Message{
+			Input: queue.Message{
 				Value: []byte("{\"video_id\":\"asasa\",\"video_title\":\"Amazing Adventure\",\"genres\":[\"Action\",\"Adventure\"],\"user_id\":12345,\"platform\":\"YouTube\",\"duration\":3600,\"timestamp\":\"1725720511\",\"video_quality\":\"4K\"}"),
 				Topic: "random",
 			},
-			err: errors.New("invalid event"),
+			Err: errors.New("invalid event"),
 		},
 		{
 			Name: "handle_event_invalid_event_2",
-			input: queue.Message{
+			Input: queue.Message{
 				Value: []byte("{\"video_id\":\"\",\"video_title\":\"Amazing Adventure\",\"genres\":[\"Action\",\"Adventure\"],\"user_id\":12345,\"platform\":\"YouTube\",\"duration\":3600,\"timestamp\":\"1725720511\",\"video_quality\":\"4K\"}"),
 				Topic: "random",
 			},
-			err: errors.New("invalid event"),
+			Err: errors.New("invalid event"),
 		},
 		{
 			Name: "handle_event_invalid_event_3",
-			input: queue.Message{
+			Input: queue.Message{
 				Value: []byte("{\"video_id\":\"asda\",\"video_title\":\"Amazing Adventure\",\"genres\":[\"Action\",\"Adventure\"],\"user_id\":0,\"platform\":\"YouTube\",\"duration\":3600,\"timestamp\":\"1725720511\",\"video_quality\":\"4K\"}"),
 				Topic: "random",
 			},
-			err: errors.New("invalid event"),
+			Err: errors.New("invalid event"),
 		},
 		{
 			Name: "handle_event_marshall_failure",
-			input: queue.Message{
+			Input: queue.Message{
 				Value:     []byte("huasgd"),
 				Topic:     "random",
 				Partition: 0,
 				Offset:    0,
 			},
-			err: errors.New("invalid JSON"),
+			Err: errors.New("invalid JSON"),
 		},
 		{
 			Name: "handle_event_marshall_failure",
-			input: queue.Message{
+			Input: queue.Message{
 				Value:     []byte(fmt.Sprintf("{\"video_id\":\"asdsa\",\"video_title\":\"Amazing Adventure\",\"genres\":[\"Action\",\"Adventure\"],\"user_id\":12345,\"platform\":\"YouTube\",\"duration\":3600,\"timestamp\":\"%d\",\"video_quality\":\"4K\"}", time.Now().Unix())),
 				Topic:     "error_topic",
 				Partition: 0,
 				Offset:    0,
 			},
-			err: errors.New("publish error"),
+			Err: errors.New("publish error"),
 		},
 	}
 	for _, tc := range testCases {
-		if msg, ok := tc.input.(queue.Message); ok {
+		if msg, ok := tc.Input.(queue.Message); ok {
 			viper.Set("message_queue.kafka.reduce-consumer.topic", msg.Topic)
 			err := handler.HandleMessage(context.TODO(), &msg)
-			assert.Equal(t, err, tc.err)
+			assert.Equal(t, err, tc.Err)
 		} else {
-			t.Error("invalid input test case format")
+			t.Error("invalid Input test case format ", tc.Name)
 		}
 	}
 
